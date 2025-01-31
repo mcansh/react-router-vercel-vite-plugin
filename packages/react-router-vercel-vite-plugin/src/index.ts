178218a5e0e9b96982a8aa3dace719a63709cce4
Logan McAnsh <logan@mcan.sh>
2025-01-30 22:14:34 -0500

    chore: enhance build process for Vercel integration
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- packages/react-router-vercel-vite-plugin/src/index.ts
+++ packages/react-router-vercel-vite-plugin/src/index.ts
@@ -28,80 +28,64 @@ export function reactRouterVercelVitePlugin({
       this.info("Clearing .vercel directory");
       await fsp.rm(".vercel", { recursive: true }).catch(() => {});
 
-      this.info("Creating .vercel/static/output directory");
-      this.info("Creating .vercel/output/functions/index.func directory");
-      await Promise.all([
-        fsp.mkdir(".vercel/output/static", { recursive: true }),
-        fsp.mkdir(".vercel/output/functions/index.func", { recursive: true }),
-      ]);
+      await fsp.mkdir(".vercel/output/static", { recursive: true });
+      this.info("Created .vercel/static/output directory");
+
+      await fsp.mkdir(".vercel/output/functions/index.func", {
+        recursive: true,
+      });
+      this.info("Created .vercel/output/functions/index.func directory");
 
       if (await fsp.stat("build/server").catch(() => null)) {
-        fsp
-          .cp("build/server", ".vercel/output/functions/index.func", {
-            recursive: true,
-          })
-          .then(() => {
-            this.info(
-              "Copied build/server to .vercel/output/functions/index.func",
-            );
-          });
+        await fsp.cp("build/server", ".vercel/output/functions/index.func", {
+          recursive: true,
+        });
+
+        this.info("Copied build/server to .vercel/output/functions/index.func");
       }
 
-      await fsp
-        .writeFile(
-          ".vercel/output/functions/index.func/package.json",
-          JSON.stringify({ type: "module" }),
-        )
-        .then(() => {
-          this.info("Wrote .vercel/output/functions/index.func/index.js");
-        });
+      await fsp.writeFile(
+        ".vercel/output/functions/index.func/package.json",
+        JSON.stringify({ type: "module" }),
+      );
 
-      await fsp
-        .writeFile(
-          ".vercel/output/functions/index.func/.vc-config.json",
-          JSON.stringify({
-            runtime: nodeJsRuntime,
-            handler: "index.js",
-            launcherType: "Nodejs",
-            shouldAddHelpers: true,
-          }),
-        )
-        .then(() => {
-          this.info(
-            "Wrote .vercel/output/functions/index.func/.vc-config.json",
-          );
-        });
+      this.info("Wrote .vercel/output/functions/index.func/package.json");
 
-      await fsp
-        .writeFile(
-          ".vercel/output/config.json",
-          JSON.stringify({
-            version: 3,
-            routes: [
-              { src: "/favicon.ico", dest: "/favicon.ico" },
-              {
-                src: "/assets/(.*)",
-                dest: "/assets/$1",
-                headers: {
-                  "Cache-Control": "public, max-age=31536000, immutable",
-                },
+      await fsp.writeFile(
+        ".vercel/output/functions/index.func/.vc-config.json",
+        JSON.stringify({
+          runtime: nodeJsRuntime,
+          handler: "index.js",
+          launcherType: "Nodejs",
+          shouldAddHelpers: true,
+        }),
+      );
+      this.info("Wrote .vercel/output/functions/index.func/.vc-config.json");
+
+      await fsp.writeFile(
+        ".vercel/output/config.json",
+        JSON.stringify({
+          version: 3,
+          routes: [
+            { src: "/favicon.ico", dest: "/favicon.ico" },
+            {
+              src: "/assets/(.*)",
+              dest: "/assets/$1",
+              headers: {
+                "Cache-Control": "public, max-age=31536000, immutable",
               },
-              { src: "/(.*)", dest: "/" },
-            ],
-          }),
-        )
-        .then(() => {
-          this.info("Wrote .vercel/output/config.json");
-        });
+            },
+            { src: "/(.*)", dest: "/" },
+          ],
+        }),
+      );
+      this.info("Wrote .vercel/output/config.json");
 
       if (await fsp.stat("build/client").catch(() => null)) {
-        await fsp
-          .cp("build/client", ".vercel/output/static", {
-            recursive: true,
-          })
-          .then(() => {
-            this.info("Copied build/client to .vercel/output/static");
-          });
+        await fsp.cp("build/client", ".vercel/output/static", {
+          recursive: true,
+        });
+        this.info("Copied build/client to .vercel/output/static");
       }
     },
   };
